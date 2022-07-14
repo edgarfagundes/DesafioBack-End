5a0de42fc6de8ba583289f6a7c0baee510cb5eb6
Edgar Fagundes <edgar.fagundes@algosoft.com.br>
2022-07-14 16:44:12 -0300

    Commit 2

--- src/main/java/com/example/desafio/controllers/ParticipanteController.java
+++ src/main/java/com/example/desafio/controllers/ParticipanteController.java
@@ -1,12 +1,11 @@
 package com.example.desafio.controllers;
 
-import com.example.desafio.models.Compromisso;
 import com.example.desafio.models.Participante;
 import com.example.desafio.models.services.ParticipanteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -26,26 +26,31 @@ public class ParticipanteController {
     ParticipanteService participanteService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Page<Participante> findAll(Pageable pageable) {
         return participanteService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Optional<Participante> findById(@PathVariable Long id) {
         return participanteService.findById(id);
     }
 
     @PostMapping("/adicionarParticipante")
+    @ResponseStatus(HttpStatus.CREATED)
     public Participante addParticipante(@RequestBody Participante participante) {
         return participanteService.save(participante);
     }
 
     @PutMapping("/updateParticipante/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public Participante updateParticipante(@PathVariable Long id,@RequestBody Participante participante) {
         return participanteService.update(id, participante);
     }
 
     @DeleteMapping("/deleteParticipante/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteParticipante(@PathVariable Long id){
         participanteService.delete(id);
     }
