9763e43439a82419b6888e06479b57ad1818c2ae
edgarfagundes <edgarfagundessss@gmail.com>
2022-07-24 23:58:39 -0300

    Commit 76545

--- src/test/java/com/example/desafio/controllers/HistoricoControllerTest.java
+++ src/test/java/com/example/desafio/controllers/HistoricoControllerTest.java
@@ -1,14 +1,5 @@
 package com.example.desafio.controllers;
 
-import com.example.desafio.models.entities.Compromisso;
-import com.example.desafio.models.entities.Historico;
-import com.example.desafio.models.entities.Localidade;
-import com.example.desafio.models.entities.Participante;
-import com.example.desafio.models.enums.Situacao;
-import com.example.desafio.models.repository.CompromissoRepository;
-import com.example.desafio.models.repository.HistoricoRepository;
-import com.example.desafio.models.repository.LocalidadeRepository;
-import com.example.desafio.models.repository.ParticipanteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
